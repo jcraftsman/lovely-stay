0042856850ebceba4bb35125acc84c22dab70bf7
jcraftsman <wassel.alazhar@gmail.com>
2024-01-19 16:46:32 +0100

    build(paas-transaction-script): bootstrap a booking api with fastapi

---
+++ lovely-stay-paas-transaction-script/src/lovely_stay_paas_transaction_script/booking_api.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from lovely_stay_paas_transaction_script.booking_transaction import book_with_commitment_to_pay
+
+app = FastAPI()
+
+
+@app.post("/bookings")
+def http_post_bookings(http_request):
+    booking_request = parse(http_request)
+    booking_confirmed = book_with_commitment_to_pay(booking_request)
+    return http_response(booking_confirmed)
+
+
+def parse(http_request):
+    pass
+
+
+def http_response(booking_confirmed):
+    pass
